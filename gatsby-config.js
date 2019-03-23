7ccc8534aaa277e83d4c7f160823047f690120f4
Pranav Ram <pranav.ram7@gmail.com>
2019-03-23 18:21:40 +0530

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,24 @@ module.exports = {
         siteSpeedSampleRate: 10
         // cookieDomain: 'pranavram.com'
       }
+    },
+    {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
     }
   ],
   pathPrefix: '/'
