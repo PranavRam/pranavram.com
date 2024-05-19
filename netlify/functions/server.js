804329109b65ba1fa0293d2fe42b537b219a2642
Pranav Ram <pranavram@pop-os.localdomain>
2024-05-19 15:23:38 +0530

    add build location

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,4 +1,3 @@
 const { createRequestHandler } = require("@remix-run/netlify");
-import * as build from "@remix-run/dev/server-build";
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+export default createRequestHandler({ build: require("../../public/build") });
