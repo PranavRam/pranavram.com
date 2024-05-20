f4d04b30416e173d8374f38e6b07823b5eb0f4ba
Pranav Ram <pranavram@pop-os.localdomain>
2024-05-20 09:59:47 +0530

    change to remix-adapter

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,3 +1,6 @@
-const { createRequestHandler } = require("@remix-run/netlify");
+const { createRequestHandler } = require("@remix-run/remix-adapter");
+
+import * as build from "@remix-run/dev/server-build"
+console.log("Routes:", build.routes);
 
 export default createRequestHandler({ build: require("./build") });
