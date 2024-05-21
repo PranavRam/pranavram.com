c9a6a4b7c4ed537d4d7be3b059375d12723c45d6
Pranav Ram <pranavram@pop-os.localdomain>
2024-05-21 21:28:38 +0530

    update vercel node

--- remix.config.js
+++ remix.config.js
@@ -36,11 +36,17 @@ module.exports = {
   // so we default back to the standard build output.
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
-  appDirectory: "app",
-  browserBuildDirectory: "public/build",
-  // serverBuildPath: "netlify/functions/build/index.js",
-  serverBuildDirectory: "netlify/functions/server/build",
-  publicPath: "/build/",
+  // serverBuildTarget: "vercel",
+  // // When running locally in development mode, we use the built in remix
+  // // server. This does not understand the vercel lambda module format,
+  // // so we default back to the standard build output.
+  // server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  // ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // browserBuildDirectory: "public/build",
+  // // serverBuildPath: "netlify/functions/build/index.js",
+  // serverBuildDirectory: "netlify/functions/server/build",
+  // publicPath: "/build/",
   mdx: async (filename) => {
     const [rehypeHighlight, rehypeExternalLinks] = await Promise.all([
       import("rehype-highlight").then((mod) => mod.default),
